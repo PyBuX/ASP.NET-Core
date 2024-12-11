3f236b223c666a50b92742f0a149ff54aee21ac0
Aleksandr Vishnevski <youtube.buiiih9228@mail.ru>
2024-12-11 10:32:41 +0300

    /

---
+++ BSUIR_VIS/BSUIR_VIS.UI/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BSUIR_VIS.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
